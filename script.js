d7500c5509d4696b243be856c0b307eab533e16a
shahnazj <shahnaz.jafari35@gmail.com>
2024-10-03 13:46:04 +0200

    slider implemented

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const menuBtn = document.querySelector('.menu-btn')
 const mainMenu = document.querySelector('#main-menu')
+const darkModeSwitch = document.querySelector('#darkmode-switch')
+const hasSetDarkMode = localStorage.getItem('darkmode')
 
-mainMenu.classList.add('hello')
-
-menuBtn.addEventListener('click', () =>{
+menuBtn.addEventListener('click', () => {
     const isExpanded = menuBtn.getAttribute('aria-expanded') === 'true'
+    
     if(isExpanded){
         menuBtn.setAttribute('aria-expanded', false)
         mainMenu.addEventListener('animationend', () => {
@@ -12,6 +13,42 @@ menuBtn.addEventListener('click', () =>{
         }, {once: true})
     }else{
         mainMenu.classList.remove('hide')
-        menuBtn.setAttribute('aria-expanded', true)
+        menuBtn.setAttribute('aria-expanded', true);
     }
 })
+
+
+
+if(hasSetDarkMode == null){
+    if(window.matchMedia('(preferes-color-schem): dark').matches){
+        enableDarkMode()
+    }else{
+        disableDarkMode()
+    }
+}else if(hasSetDarkMode == 'on'){
+    enableDarkMode()
+}else if(hasSetDarkMode == 'off'){
+    disableDarkMode()
+}
+
+
+
+darkModeSwitch.addEventListener('change', () => {
+    if(darkModeSwitch.checked){
+        document.documentElement.classList.add('dark')
+        localStorage.setItem('darkMode', 'on')
+    }else{
+        document.documentElement.classList.remove('dark')
+        localStorage.setItem('darkMode', 'off')
+    }
+})
+
+function enableDarkMode(){
+    darkModeSwitch.checked = true
+    document.documentElement.classList.add('dark')
+}
+
+function disableDarkMode(){
+    darkModeSwitch.checked = false
+    document.documentElement.classList.remove('dark')
+}
